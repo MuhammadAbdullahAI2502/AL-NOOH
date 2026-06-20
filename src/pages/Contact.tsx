c8d30cbfb6ced6c88f7dfeaab5604654214ccdaf
Muhammad Abdullah <abdullahgenai12@gmail.com>
2026-06-20 20:35:50 +0500

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -323,16 +323,16 @@ export default function Contact({ lang }: ContactProps) {
             {isAr ? "موقعنا" : "Our Location"}
           </h2>
           <div className="w-full h-80 bg-brand-green-950/10 rounded-lg overflow-hidden shadow-lg">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3427.039795891672!2d72.26335477558312!3d30.801518274551125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzDCsDQ4JzA1LjUiTiA3MsKwMTUnNTcuNCJF!5e0!3m2!1sen!2s!4v1781968941945!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
-              width="100%"
-              height="100%"
-              style={{ border: 0 }}
-              allowFullScreen
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              className="w-full h-full"
-            ></iframe>
+           <iframe
+            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3427.039795891672!2d72.26335477558312!3d30.801518274551125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzDCsDQ4JzA1LjUiTiA3MsKwMTUnNTcuNCJF!5e0!3m2!1sen!2s!4v1781968941945!5m2!1sen!2s"
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            className="w-full h-full"
+            />
           </div>
         </div>
       </section>
